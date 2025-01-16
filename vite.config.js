ab38db508fb76a3204ee040b67ed94ea58a3dafd
Damian Ożóg <dam.ozog@gmail.com>
2025-01-16 16:35:41 +0100

    refactor code in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Wymagane przez Vercel
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000", // Lokalny backend podczas developmentu
+    },
+  },
+});
